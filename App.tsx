4c6883631b8b7e3f8a073687809e8cf7778d6e5c
Felipe Amaral <felipeamaraldev15@gmail.com>
2022-07-18 00:08:06 -0300

    Aula 1

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NativeBaseProvider, StatusBar } from "native-base";
+import { SignIn } from './src/screens/SignIn';
+import { THEME } from './src/styles/theme';
+import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto';
+import { Loading } from './src/components/Loading';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar barStyle='light-content' backgroundColor="transparent" translucent />
+      { fontsLoaded ? <SignIn /> : <Loading /> }
+    </NativeBaseProvider>  
+  );
+}
+
---
+++ src/components/Input.tsx
@@ -0,0 +1,24 @@
+import { Input as NatibeBaseInput, IInputProps } from 'native-base';
+
+export function Input({...rest}: IInputProps) {
+  return (
+    <NatibeBaseInput 
+        bg="gray.700"
+        h={14}
+        size="md"
+        borderWidth={0}
+        fontSize="md"
+        fontFamily="body"
+        color="white"
+        placeholderTextColor="gray.300"
+        {...rest}
+        _focus={{
+            borderWidth: 1,
+            borderColor: "green.500",
+            bg: "gray.700"
+        }}
+    />
+
+    
+  );
+}

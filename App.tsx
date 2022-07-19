9cf97d39cff28bfdc0dfc71a6eeba7cc5e81d028
Felipe Amaral <felipeamaraldev15@gmail.com>
2022-07-18 23:05:07 -0300

    finalizada tela de registro

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import { THEME } from './src/styles/theme';
 import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto';
 import { Loading } from './src/components/Loading';
 import { Home } from './src/screens/Home';
+import { Register } from './src/screens/Register';
 
 export default function App() {
   const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
@@ -12,7 +13,7 @@ export default function App() {
   return (
     <NativeBaseProvider theme={THEME}>
       <StatusBar barStyle='light-content' backgroundColor="transparent" translucent />
-      { fontsLoaded ? <Home /> : <Loading /> }
+      { fontsLoaded ? <Register /> : <Loading /> }
     </NativeBaseProvider>  
   );
 }
